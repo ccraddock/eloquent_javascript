5975d45cd92f6330236a8642b9e628af51def419
Cameron Craddock <cameron.craddock@gmail.com>
2021-09-27 21:17:36 -0500

    first import

---
+++ ch1/ex1.js
@@ -0,0 +1,6 @@
+let symbol_string = "#";
+
+while(symbol_string.length <= 7) {
+    console.log(symbol_string);
+    symbol_string += "#";
+} 
---
+++ ch1/ex3.js
@@ -0,0 +1,15 @@
+let size = 10;
+let testString = '';
+
+for(let outerIndex = 0; outerIndex < size; outerIndex++){
+    let evenOdd = outerIndex % 2;
+
+    for(let innerIndex = 0; innerIndex < size; innerIndex++){
+        testString += innerIndex % 2 === evenOdd ? " " : "#"; 
+    }
+    testString += "\n";
+}
+
+console.log(testString);
+
+console.log(testString);
